961810a501fee509e5e29fea7d779fcac29cfafe
Alistair Smith <alistairsmith01@gmail.com>
2020-11-04 03:14:53 +0000

    Move index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { join } from "path";
 import { WS } from "./WS";
 
-const path = join(__dirname, "index.html");
+const path = join(__dirname, "..", "static", "index.html");
 
 const app = express();
 
